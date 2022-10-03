fdf2d6b75292d11cd2a375ce4269a709fe3a1b0f
William Bernoudy <william@bernoudy.xyz>
2022-10-03 11:52:09 -0700

    Nvim tree config

--- lua/master-plugins.lua
+++ lua/master-plugins.lua
@@ -102,7 +102,13 @@ return require('packer').startup({function(use)
       'kyazdani42/nvim-tree.lua',
       requires = 'kyazdani42/nvim-web-devicons',
       config = function()
-          require'nvim-tree'.setup {}
+          require('nvim-tree').setup({
+            actions = {
+                open_file = {
+                    resize_window = false,
+                }
+            }
+          })
           require('plugin-settings.nvimtree-config')
       end
   }
